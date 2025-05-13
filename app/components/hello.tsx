d683e058b9884f5caa6158040c7f4c34319d5bc7
ammar-coh <ammarkhn91@gmail.com>
2025-05-13 07:37:33 +0500

    intiial work dynamic/nested/basic routing server/client components etc

---
+++ app/components/hello.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+
+function Hello() {
+  console.log('hello iam from client')
+  return (
+    <div>hello iam from client
+      {/* <h1>hello iam from client</h1> */}
+    </div>
+  )
+}
+
+export default Hello
